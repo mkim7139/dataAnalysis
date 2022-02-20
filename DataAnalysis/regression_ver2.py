b288ec6e585549f6eeb426ba22b416aa2c662238
mkim7139 <90369493+mkim7139@users.noreply.github.com>
2022-02-20 00:25:25 -0800

    Update regression_ver2.py

--- DataAnalysis/regression_ver2.py
+++ DataAnalysis/regression_ver2.py
@@ -1,37 +1,46 @@
 # regression_ver2.py (version 2)
 # Max Kim
 #
-# Takes data and an expected trend. Generates a polynomial
-# fit curve using gradient descent.
+# Takes data and an expected trend (linear, quadratic, cubic,...)
+# Generates a polynomial fit curve using gradient descent.
 
 # import libraries
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.pyplot import plot
 
-# creates a cubic curve given a domain and coefficients.
-def cube(x, a, b, c, d):
-    return a + b*x + c*x**2 + d*x**3
+# import powerRule_ver3 file to calculate derivatives
+from powerRule_ver3 import *
+derive('x^3 -1x^(-4)', 2)
+
+def polynomial(x, coefs):
+    coefs=coefs.astype('float64')
+    func=0
+    for i in range(len(coefs)):
+        func+=coefs[i] * (x**i)
+    return func
 
 # creates a fit curve using gradient descent.
 # takes data arrays and guess coefficients, returns fit curve coefficient.
-def fit_curve(x, y, a, b, c, d):
+def fit_curve(x, y, coefs):
+    coefs=coefs.astype('float64')
     n = len(x)
-    plot(x, cube(x, a, b, c, d), color='k', alpha=.3)
-    for i in range(350):
-        # h = a + bx + cx^2 + dx^3
-        d1 = sum(a + b*x + c*x**2 + d*x**3 - y) * 2/n       #partial derivs
-        d2 = sum((a + b*x + c*x**2 + d*x**3 - y)*x) * 2/n
-        d3 = sum((a + b*x + c*x**2 + d*x**3 - y)*x**2) * 2/n
-        d4 = sum((a + b*x + c*x**2 + d*x**3 - y)*x**3) * 2/n
-        a-= .001*d1       #learning rates
-        b-= .001*d2
-        c-= .001*d3
-        d-= .00001*d4
+    plot(x, polynomial(x, coefs), color='g', alpha=.5)
+    for i in range(900):
+        # error = (a + bx + cx^2 + dx^3)^2 / n
+        d1 = sum(coefs[0] + coefs[1]*x + coefs[2]*x**2 + coefs[3]*x**3 - y) * 2/n       #partial derivs
+        d2 = sum((coefs[0] + coefs[1]*x + coefs[2]*x**2 + coefs[3]*x**3 - y)*x) * 2/n
+        d3 = sum((coefs[0] + coefs[1]*x + coefs[2]*x**2 + coefs[3]*x**3 - y)*x**2) * 2/n
+        d4 = sum((coefs[0] + coefs[1]*x + coefs[2]*x**2 + coefs[3]*x**3 - y)*x**3) * 2/n
+
+        coefs[0]-= .001*d1       #learning rates
+        coefs[1]-= .0001*d2
+        coefs[2]-= .0001*d3
+        coefs[3]-= .00001*d4
         if i % 2 ==0:
-            plot(x, cube(x, a, b, c, d), color='k', alpha=.3)
-    plot(x, cube(x, a, b, c, d))
-    return[a, b, c, d]
+            plot(x, polynomial(x, coefs), color='k', alpha=.3)
+    plot(x, polynomial(x, coefs))
+    return coefs
 
 
 # generate axes
@@ -39,7 +48,8 @@ plt.axis([-10, 10, -300, 300])
 
 #generate curve
 x = np.linspace(-7, 7, 80)
-c1 = cube(x, -2, 3, -5, -4)
+c1_coefs=np.array([-2, 3, -5, -4])
+c1 = polynomial(x, c1_coefs)
 
 #generate noise
 noise_ctr = 0
@@ -50,10 +60,12 @@ noise = np.random.normal(noise_ctr, noise_std, len(x))
 n_c1 = c1 + noise
 
 # generate fit curve coefficients
-fit = fit_curve(x, n_c1, 1, 1, 1, 1)
+starting_coefs=np.array([1, 1, 1, 1])
+fit_coefs = fit_curve(x, n_c1, starting_coefs)
+
 
 # plotting curves
 plot(x, c1, linewidth='1')
 plot(x, n_c1, '.')
-plot(x, cube(x, fit[0], fit[1], fit[2], fit[3]), color='blue')
+plot(x, polynomial(x, fit_coefs), color='blue')
 plt.show()
